955d8c6ccd3311d6f1551da6fcd3859d1f958886
nicogalii <galizianicolo2004@gmail.com>
2024-10-27 23:58:45 +0100

    delete/

--- esercizi/galizia-nicolo-units-15/15-bootstrap-components/1-boot-me-up-scotty/script/main.js
+++
@@ -1,5 +0,0 @@
-// JavaScript to show feedback alert
-document.querySelector('form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    document.getElementById('feedback').style.display = 'block';
-});
